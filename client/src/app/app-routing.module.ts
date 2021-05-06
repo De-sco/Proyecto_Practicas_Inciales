6681389642277113583e43f47ccd92a7e9ebe30f
byron <solisbyron711@gmail.com>
2021-05-05 22:01:52 -0600

    Se crearon las secciones de login y registro de usuarios

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Acá importamos los html de los componestes como import{ nombre_con_el_que_nos_vamos_a_referir_a_ellos}
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: 'registro',
+    component: UserFormComponent
+  }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
